e27e457ab4c0153febbc04155df90aded64773b0
Your Name <you@example.com>
2022-06-09 23:37:27 +0100

    tsql update

--- ch3.ps1
+++ ch3.ps1
@@ -1,1 +1,9 @@
 #   The dbatools lab    
+# Install-DbaInstance -Version 2019 -SqlInstance localhost\sql2019 -Feature Engine -Path C:\dev\06\powershell\sqlserver\2019 -AuthenticationMode Mixed
+# Install-DbaInstance -Version 2017 -SqlInstance localhost\sql2017 -Feature Engine -Path C:\dev\06\powershell\sqlserver\2017 -AuthenticationMode Mixed
+
+Find-DbaInstance -ComputerName localhost
+
+## Create test logins and jobs
+
+
