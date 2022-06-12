33ef85fcfaec2b241b5c8c1bb83d8e224c978101
Your Name <you@example.com>
2022-06-12 06:32:54 +0100

    filter, sort

--- oop/Creating Custom Objects.ps1
+++ oop/Creating Custom Objects.ps1
@@ -31,7 +31,38 @@ Get-Process | where -Property ProcessName  -match -Value "^p.*"
 ## filter object wildchar
 
 Get-module -ListAvailable | where {$_.name -NotLike "microsoft*" -and $_.name -notlike 'ps*'}
-Get-module -ListAvailable | where name -Like "microsoft*"
+## Get-module -ListAvailable | where name -Like "microsoft*" -and $_.name -notlike 'ps*' not working
+
+# Sort
+Get-ChildItem C:\dev\06 | Sort-Object
+Get-ChildItem C:\dev\06 | Sort-Object -Property Length
+Get-ChildItem C:\dev\06\powershell\PowerShellCookbook | Sort-Object -Property Length
+Get-ChildItem C:\dev\06\powershell\PowerShellCookbook | Sort-Object -Property Length | select -First 10
+Get-ChildItem C:\dev\06\powershell\PowerShellCookbook | Sort-Object -Property Length -Descending | select -First 10
+
+Get-Service | Sort-Object -Property @{
+    Expression="status";
+    Descending=$true  # note true with $true
+}
+Get-Service | Sort-Object -Property @{
+    Expression="status";
+    Descending=$true  # note true with $true
+}, @{
+    Expression="displayname";
+    Descending=$false  # note true with $true
+}
+
+## sort-object unique
+Get-Content c:\powershell\test.txt | sort-object {[int]$_}
+Get-Content c:\powershell\test.txt | sort-object -Property @{
+    Expression={$_.CreationTime - $_.LastWriteTime};
+    Descending=$false
+} | Format-Table CreationTime,LastWriteTime,FullName
+
+
+
+
+
 
 
 
